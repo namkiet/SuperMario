dbe4a6069c498a296918a010523d5ac9bf2643dc
Tranhau346 <trunghaulx03042006@gmail.com>
2025-08-10 12:38:15 +0700

    Change Piranha patrol

--- include/Gameplay/Enemy/Piranha/Piranha.hpp
+++ include/Gameplay/Enemy/Piranha/Piranha.hpp
@@ -23,7 +23,7 @@ public:
         addComponent<Animation>(Animation(TextureManager::load("assets/Enemy/Piranha/piranha.png"), 16, 24, 2, 0.25f));
         getComponent<Animation>().zIndex = -1;
         addComponent<EnemyTag>(std::make_shared<PiranhaNormalState>(), std::make_shared<PiranhaBehaviour>());
-        addComponent<PiranhaPatrol>(24 * 3 + 16, 2.0f, 2.0f);
+        addComponent<PiranhaPatrol>(y * scale, 24 * 3 + 16, 2.0f, 2.0f);
         addComponent<PassThroughTag>();
 
         std::vector<Direction> directions = {Direction::Left, Direction::Right, Direction::Top, Direction::Bottom};
--- src/Enemy/Piranha/PiranhaBehaviour.cpp
+++ src/Enemy/Piranha/PiranhaBehaviour.cpp
@@ -5,6 +5,7 @@
 #include <Engine/Physics/BoxCollider2D.hpp>
 #include <Engine/Physics/BlockTag.hpp>
 #include <Engine/Core/RigidBody.hpp>
+#include <Engine/Core/Transform.hpp>
 #include <Engine/Core/DespawnTag.hpp>
 #include <Engine/Audio/Components.hpp>
 #include <Engine/Audio/SoundManager.hpp>
@@ -37,19 +38,30 @@ void PiranhaBehaviour::patrol(Entity* entity, float dt, World& world)
 
     auto& patrol = entity->getComponent<PiranhaPatrol>();
     auto& rb = entity->getComponent<RigidBody>();
+    auto& tf = entity->getComponent<Transform>();
 
-    patrol.timer += dt;
-    if (patrol.timer > patrol.phaseDuration + patrol.delay)
+    if (tf.position.y <= patrol.minHeight)
     {
-        patrol.timer -= patrol.phaseDuration + patrol.delay;
+        tf.position.y = patrol.minHeight;
+        patrol.timer += dt;
+    }
+    else if (tf.position.y >= patrol.maxHeight)
+    {
+        tf.position.y = patrol.maxHeight;
+        patrol.timer += dt;
+    }
+
+    if (patrol.timer >= patrol.delay)
+    {
+        patrol.timer = 0;
         patrol.velocity.y *= -1;
     }
 
-    if (patrol.timer <= patrol.phaseDuration)
+    if (patrol.timer == 0)
     {
         rb.velocity = patrol.velocity;
     }
-    else 
+    else
     {
         rb.velocity = sf::Vector2f(0, 0);
     }
