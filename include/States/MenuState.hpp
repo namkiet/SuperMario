5ef3e8c720438bd39b1fd9d1e1375266592cc566
AkiyaNguyen <pn4358189@gmail.com>
2025-08-14 22:01:23 +0700

    separate menustate from UI

--- include/States/MenuState.hpp
+++ include/States/MenuState.hpp
@@ -21,23 +21,5 @@ public:
     void handleEvent(const sf::Event &event) override;
     void update(float dt) override;
     void render(sf:: RenderWindow&window) override;
-private:
-    void drawPlayComponent();
-    std::shared_ptr<Button> makeButton(
-        sf::Vector2f pos,
-        std::shared_ptr<sf::Shape> shape,
-        const std::string& label,
-        const StateColor& colors,
-        bool toggle,
-        unsigned int textSize,
-        std::function<void()> onClick = nullptr,
-        std::shared_ptr<sf::Sprite> sprite = nullptr,
-        sf::Color textColor = sf::Color::White
-    );
-    std::shared_ptr<StaticComponent> makeText(
-        const std::string& content,
-        sf::Vector2f pos,
-        int charSize,
-        const StateColor& colorSetting
-    );
+
 };
